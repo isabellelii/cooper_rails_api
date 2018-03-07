db4ceee4187975fe827f4ea2faec32ed8f9f0af0
kalle <kmthorsnes@gmail.com>
2018-03-07 15:59:05 +0100

    for code debugging

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe User, type: :model do
     end
 
     describe 'Database table' do
-        it { is_expected. to have_db_column :id }
+        it { is_expected.to have_db_column :id }
         it { is_expected.to have_db_column :provider }
         it { is_expected.to have_db_column :uid }
         it { is_expected.to have_db_column :encrypted_password }
@@ -31,16 +31,16 @@ RSpec.describe User, type: :model do
     end
 
     describe 'Validations' do
-        it { is_expected.to validate_presence_of(:email) }
-        it { is_expected.to validate_confirmation_of(:password) }
+      it { is_expected.to validate_presence_of(:email) }
+      it { is_expected.to validate_confirmation_of(:password) }
 
-    context 'should not have an invalid email address' do
-      emails = ['asdf@ ds.com', '@example.com', 'test me @yahoo.com',
-                'asdf@example', 'ddd@.d. .d', 'ddd@.d']
+      context 'should not have an invalid email address' do
+        emails = ['asdf@ ds.com', '@example.com', 'test me @yahoo.com',
+                  'asdf@example', 'ddd@.d. .d', 'ddd@.d']
 
-      emails.each do |email|
-        it { is_expected.not_to allow_value(email).for(:email) }
-      end
+        emails.each do |email|
+          it { is_expected.not_to allow_value(email).for(:email) }
+        end
     end
 
     context 'should have a valid email address' do
