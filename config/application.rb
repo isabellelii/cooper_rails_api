90f53ed8aef86b6d23b13e21c3bfdcaf657b6856
kalle <kmthorsnes@gmail.com>
2018-03-09 13:02:19 +0100

    modified application.rb

--- config/application.rb
+++ config/application.rb
@@ -19,13 +19,10 @@ module CooperApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', 
-          headers: :any, 
-          methods: %i[get post put delete],
-          expose: %w(access-token expiry token-type uid client),
-          max_age: 0
+        resource '*', headers: :any, methods: [:get, :post, :put, :delete],
+                 expose: %w(access-token expiry token-type uid client),
+                 max_age: 0
       end
     end
   end
 end
-
